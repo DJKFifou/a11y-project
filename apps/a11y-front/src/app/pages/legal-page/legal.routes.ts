e9a8af1b1376383d8199b94afd86ec221fc6ee21
Maxime Lust <maxime@favoreatdesign.com>
2025-03-27 10:38:46 +0100

    add legal-notices & confidentiality pages

---
+++ apps/a11y-front/src/app/pages/legal-page/legal.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { LegalPageComponent } from './legal-page.component';
+
+export const legalRoutes: Route[] = [
+  {
+    path: '',
+    component: LegalPageComponent,
+  },
+];
